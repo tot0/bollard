d087c1938fb358d587a12f1439c82d697b1fc58b
Tao Wang <tawan@microsoft.com>
2022-06-24 18:59:44 +0000

    Remove some unit tests

--- tests/swarm_test.rs
+++ tests/swarm_test.rs
@@ -11,6 +11,7 @@ pub mod common;
 use crate::common::*;
 
 async fn swarm_test(docker: Docker) -> Result<(), Error> {
+    // init swarm
     let config = InitSwarmOptions {
         listen_addr: "0.0.0.0:2377",
         advertise_addr: "127.0.0.1",
@@ -19,75 +20,23 @@ async fn swarm_test(docker: Docker) -> Result<(), Error> {
         .init_swarm(config)
         .await?;
 
+    // inspect swarm
     let inspection_result = &docker
         .inspect_swarm()
         .await?;
-    
     assert!(inspection_result.join_tokens.as_ref().unwrap().worker.as_ref().unwrap().len() > 0);
 
+    // leave swarm
     let config = LeaveSwarmOptions {
         force: true,
     };
     let _ = &docker
         .leave_swarm(Some(config))
         .await?;
-    Ok(())
-}
-
-async fn inspect_swarm_test(docker: Docker) -> Result<(), Error> {
-    let config = InitSwarmOptions {
-        listen_addr: "0.0.0.0:2377",
-        advertise_addr: "127.0.0.1",
-    };
-    let _ = &docker
-        .init_swarm(config)
-        .await?;
-    
-    let result = &docker
-        .inspect_swarm()
-        .await?;
-    print!("init {}", result.join_tokens.as_ref().unwrap().worker.as_ref().unwrap());
-    Ok(())
-}
-
-async fn join_swarm_test(docker: Docker) -> Result<(), Error> {
-    let config = JoinSwarmOptions {
-        advertise_addr: "127.0.0.1",
-        join_token: "SWMTKN-1-4ssgk9ow7an36mjc0oh7nj5z10c036igse5nlxusq9j28nbz86-dftyw9ad7qgnp8lm5riwgs2ibtest",
-    };
-    let _ = &docker
-        .join_swarm(config)
-        .await?;
-    Ok(())
-}
-
-async fn leave_swarm_test(docker: Docker) -> Result<(), Error> {
-    let config = LeaveSwarmOptions {
-        force: true,
-    };
-    let _ = &docker
-        .leave_swarm(Some(config))
-        .await?;
-
     Ok(())
 }
 
 #[test]
 fn integration_test_swarm() {
     connect_to_docker_and_run!(swarm_test);
-}
-
-#[test]
-fn integration_test_inspect_swarm() {
-    connect_to_docker_and_run!(inspect_swarm_test);
-}
-
-#[test]
-fn integration_test_join_swarm() {
-    connect_to_docker_and_run!(join_swarm_test);
-}
-
-#[test]
-fn integration_test_leave_swarm() {
-    connect_to_docker_and_run!(leave_swarm_test);
 }
